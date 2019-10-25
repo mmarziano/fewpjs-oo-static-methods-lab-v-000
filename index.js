445aaa41952427f32cbbeda460c3bb89216729a3
Monica Marziano <mmarziano1@gmail.com>
2019-10-25 22:41:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Formatter {
     let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
     let s = sentence.split(' ')
     s.map(function(word) {
-       if (s !== arr.find(word => word === s)) {
+       if (word !== arr.find(s => word === s)) {
         s.charAt(0).toUpperCase() + str.slice(1)
       }
     })
