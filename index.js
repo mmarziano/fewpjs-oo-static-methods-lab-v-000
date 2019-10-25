919fe203404ce8bdbe9f16db2351492081662b23
Monica Marziano <mmarziano1@gmail.com>
2019-10-25 20:43:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+ 
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  
+  
+  static titleize(sentence) {
+    let arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let s = sentence.split(' ')
+      if (s !== arr.find(s)) {
+        s.charAt(0).toUpperCase() + str.slice(1)
+      }
+    console.log(s)
+    
+    
+    
+  }
+  
 }
